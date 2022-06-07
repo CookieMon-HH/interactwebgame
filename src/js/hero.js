cc860f44b4fad2b8940aba1fd70b0500fab91c5f
seong954t <seomtae@naver.com>
2022-06-08 01:29:12 +0900

    feature: tsc 추가

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,108 +1,95 @@
 class HeroRender {
-  el;
-  constructor(el) {
-    this.el = document.querySelector(el);
-  }
-  left() {
-    this.el.classList.add("run");
-    this.el.classList.add("flip");
-  }
-  right() {
-    this.el.classList.add("run");
-    this.el.classList.remove("flip");
-  }
-  moveEnd() {
-    this.el.classList.remove("run");
-  }
-  attack() {
-    this.el.classList.add("attack");
-  }
-  attackEnd() {
-    this.el.classList.remove("attack");
-  }
-  render(movex) {
-    this.el.parentNode.style.transform = `translateX(${movex}px)`;
-  }
-  position() {
-    const { left, right, top, height } = this.el.getBoundingClientRect();
-    return {
-      left,
-      right,
-      top: gameProp.screenHeight - top,
-      bottom: gameProp.screenHeight - top - height,
-    };
-  }
-  size() {
-    return {
-      width: this.el.offsetWidth,
-      height: this.el.offsetHeight,
-    };
-  }
+    constructor(el) {
+        this.el = document.querySelector(el);
+    }
+    left() {
+        this.el.classList.add("run");
+        this.el.classList.add("flip");
+    }
+    right() {
+        this.el.classList.add("run");
+        this.el.classList.remove("flip");
+    }
+    moveEnd() {
+        this.el.classList.remove("run");
+    }
+    attack() {
+        this.el.classList.add("attack");
+    }
+    attackEnd() {
+        this.el.classList.remove("attack");
+    }
+    render(movex) {
+        this.el.parentNode.style.transform = `translateX(${movex}px)`;
+    }
+    position() {
+        const { left, right, top, height } = this.el.getBoundingClientRect();
+        return {
+            left,
+            right,
+            top: gameProp.screenHeight - top,
+            bottom: gameProp.screenHeight - top - height,
+        };
+    }
+    size() {
+        return {
+            width: this.el.offsetWidth,
+            height: this.el.offsetHeight,
+        };
+    }
 }
-
 class Hero {
-  render;
-  movex;
-  speed;
-  bulletComProp;
-  constructor(render) {
-    this.render = render;
-    this.movex = 0;
-    this.speed = 16;
-    this.bulletComProp = {
-      launch: false,
-      arr: [],
-    };
-  }
-  keyMotion(key) {
-    if (key.keyDown["left"]) {
-      this.render.left();
-      this.left();
-    } else if (key.keyDown["right"]) {
-      this.render.right();
-      this.right();
+    constructor(render) {
+        this.render = render;
+        this.movex = 0;
+        this.speed = 16;
+        this.bulletComProp = {
+            launch: false,
+            arr: [],
+        };
     }
-
-    if (key.keyDown["attack"]) {
-      if (!this.bulletComProp.launch) {
-        this.render.attack();
-        this.attack();
-      }
+    keyMotion(key) {
+        if (key.keyDown["left"]) {
+            this.render.left();
+            this.left();
+        }
+        else if (key.keyDown["right"]) {
+            this.render.right();
+            this.right();
+        }
+        if (key.keyDown["attack"]) {
+            if (!this.bulletComProp.launch) {
+                this.render.attack();
+                this.attack();
+            }
+        }
+        if (!key.keyDown["left"] && !key.keyDown["right"]) {
+            this.render.moveEnd();
+        }
+        if (!key.keyDown["attack"]) {
+            this.render.attackEnd();
+            this.attackEnd();
+        }
+        this.render.render(this.movex);
     }
-
-    if (!key.keyDown["left"] && !key.keyDown["right"]) {
-      this.render.moveEnd();
+    left() {
+        this.movex = this.movex - this.speed;
     }
-
-    if (!key.keyDown["attack"]) {
-      this.render.attackEnd();
-      this.attackEnd();
+    right() {
+        this.movex = this.movex + this.speed;
     }
-
-    this.render.render(this.movex);
-  }
-
-  left() {
-    this.movex = this.movex - this.speed;
-  }
-  right() {
-    this.movex = this.movex + this.speed;
-  }
-  attack() {
-    const { left, bottom } = this.render.position();
-    const { width, height } = this.render.size();
-    const x = left + width / 2;
-    const y = bottom - height / 2;
-
-    this.bulletComProp.arr.push(new Bullet(x, y));
-
-    this.bulletComProp.launch = true;
-  }
-  attackEnd() {
-    this.bulletComProp.launch = false;
-  }
-
-  get bullets() {
-    return this.bulletComProp.arr;
-  }
+    attack() {
+        const { left, bottom } = this.render.position();
+        const { width, height } = this.render.size();
+        const x = left + width / 2;
+        const y = bottom - height / 2;
+        this.bulletComProp.arr.push(new Bullet(x, y));
+        this.bulletComProp.launch = true;
+    }
+    attackEnd() {
+        this.bulletComProp.launch = false;
+    }
+    get bullets() {
+        return this.bulletComProp.arr;
+    }
 }
---
+++ src/ts/bullet.ts
@@ -0,0 +1,36 @@
+class BulletRender {
+  parentNode;
+  el;
+
+  constructor() {
+    this.parentNode = document.querySelector(".game");
+    this.el = document.createElement("div");
+    this.el.className = "hero_bullet";
+    this.parentNode.appendChild(this.el);
+  }
+
+  render(x, y) {
+    this.el.style.transform = `translate(${x}px, ${y}px)`;
+  }
+}
+
+class Bullet {
+  render;
+  x: number;
+  y: number;
+  speed: number;
+  distance: number;
+  
+  constructor(x, y) {
+    this.render = new BulletRender();
+    this.x = x;
+    this.y = y;
+    this.speed = 30;
+    this.distance = x;
+    this.render.render(x, y);
+  }
+  moveBullet() {
+    this.distance += this.speed;
+    this.render.render(this.distance, this.y);
+  }
+}
