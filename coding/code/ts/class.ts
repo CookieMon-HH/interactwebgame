3e857bc5f1e56101f3c78c1ab4f0c1f5a7a074f4
CookieMon-HH <lhh10133@gmail.com>
2022-06-12 18:23:37 +0900

    section1 마무리
    Section1
    1. 배경 추가 및 bullet 삭제 등 추가
    2. typescript 반영

--- coding/code/ts/class.ts
+++ coding/code/ts/class.ts
@@ -1,10 +1,20 @@
 var degtorad = (deg) => {
-    let rad = 0.5 * Math.PI - (Math.PI / 180 * deg);
+    let rad: number = 0.5*Math.PI - (Math.PI/180*deg);
     //+y출발 시계 방향의 deg를 +x출발 반시계방향의 rad로 변환
-    return rad;
-};
+    return rad
+}
+
 class Tank {
-    constructor(el) {
+    el: any ;
+    rotateangle: number;
+    movex: number;
+    movey: number;
+    speed: number;
+    directionstatus: string;
+    deltaX: number;
+    deltaY: number;
+
+    constructor(el){
         this.el = document.querySelector(el);
         this.rotateangle = 0;
         this.movex = 0;
@@ -14,96 +24,113 @@ class Tank {
         //다른이름(basicfunction)으로 instance로 만들면 안되고 같은 이름으로 만들때만 되네?.. 왜그러지?
     }
     // constructor 다시 공부해보자
-    checkdirection() {
-        let keydir = ['up', 'left', 'down', 'right'];
-        let directionlist = ['up', 'up_left', 'left', 'left_down', 'down', 'down_right', 'right', 'right_up'];
-        let direction = 'notmoving';
-        for (let i = 0; i < keydir.length; i++) {
-            if (key.keyDown[keydir[i]]) {
+
+    checkdirection(){ 
+        let keydir: string[] = ['up','left','down','right'];
+        let directionlist: string[] = ['up','up_left','left','left_down','down','down_right','right','right_up'];
+        let direction: string = 'notmoving';
+        
+        for (let i=0; i< keydir.length; i++) {
+            if (key.keyDown[keydir[i]]){
                 //[참고]==true/false로 하면 undefined인 경우 인식을 못하는데 이렇게 하면 undefined도 인식하여 진행할 수 있음
-                if (key.keyDown[(i <= 2) ? keydir[i + 1] : keydir[0]]) {
-                    direction = directionlist[i * 2 + 1];
+                if(key.keyDown[ (i<=2) ? keydir[i+1] : keydir[0] ]){
+                    direction = directionlist[i*2+1];
                     this.directionstatus = direction;
-                    return direction;
-                }
-                else if (!key.keyDown[(i <= 2) ? keydir[i + 1] : keydir[0]]) {
-                    if (i == 0 && key.keyDown[keydir[keydir.length - 1]]) {
-                        direction = direction = directionlist[directionlist.length - 1];
+                    return direction
+                }else if (!key.keyDown[(i<=2) ? keydir[i+1] : keydir[0]]){
+                    if (i==0 && key.keyDown[keydir[keydir.length-1]]) {
+                        direction = direction = directionlist[directionlist.length-1];
                         this.directionstatus = direction;
-                        return direction;
-                    }
-                    else {
-                        direction = directionlist[i * 2];
+                        return direction
+                    }else {
+                        direction = directionlist[i*2];
                         this.directionstatus = direction;
-                        return direction;
+                        return direction
                     }
                 }
             }
         }
-        return direction;
+        return direction
     }
-    keyMotion() {
+
+    keyMotion(){                
         //탱크 이동 동작
-        if (this.checkdirection() != 'notmoving') {
+        if(this.checkdirection() != 'notmoving'){
             this.el.classList.add('move');
             // Element.classList.add/remove/toggle를 통해 명시된 class를 추가/제거/onoff할 수 있다. 
             this.rotateangle = rotateanglelist[this.checkdirection()];
+            
             this.deltaX = this.speed * Math.cos(degtorad(this.rotateangle));
             this.deltaY = -1 * this.speed * Math.sin(degtorad(this.rotateangle));
             this.movex = this.movex < 0 ? 0 : this.movex + this.deltaX;
             this.movey = this.movey > 0 ? 0 : this.movey + this.deltaY;
-        }
-        else {
+        }else {
             this.el.classList.remove('move');
         }
+
         //attack
-        if (key.keyDown['attack']) {
-            if (!bulletComProp.launch) {
+        if(key.keyDown['attack']){
+            if(!bulletComProp.launch){
                 this.el.classList.add('attack');
                 bulletComProp.arr.push(new Bullet);
+
                 bulletComProp.launch = true;
-            }
+            }   
         }
-        if (!key.keyDown['attack']) {
+        
+        if(!key.keyDown['attack']){
             this.el.classList.remove('attack');
             bulletComProp.launch = false;
-        }
+        }   
+
         this.el.style.transform = `rotate(${this.rotateangle}deg)`;
         this.el.parentNode.style.transform = `translate3d(${this.movex}px, ${this.movey}px , 0)`;
     }
-    position() {
+    position(){
         return {
             left: this.el.getBoundingClientRect().left,
             right: this.el.getBoundingClientRect().right,
             top: gameProp.screenHeight - this.el.getBoundingClientRect().top,
             bottom: gameProp.screenHeight - this.el.getBoundingClientRect().top - this.el.getBoundingClientRect().height
-        };
+        }
     }
-    size() {
+    size(){
         return {
             width: this.el.offsetWidth,
             height: this.el.offsetHeight
-        };
+        }
     }
 }
+
 class Bullet {
-    constructor() {
-        this.parentNode = document.querySelector('.game');
-        this.el = document.createElement('div');
-        this.el.className = 'tank_bullet';
+    parentNode;
+    el;
+    x: number;
+    y: number;
+    speed: number;
+    distancex: number;
+    distancey: number;
+    rotateangle: number;
+    
+    constructor(){
+		this.parentNode = document.querySelector('.game');
+		this.el = document.createElement('div');
+		this.el.className = 'tank_bullet';
+        
         // this.bl = document.createElement('div');
         // this.bl.className = 'tank_bullet';
+		
         this.x = 0;
-        this.y = 0;
-        this.speed = 30;
-        this.distancex = 0;
+		this.y = 0;
+		this.speed = 30;
+		this.distancex = 0;
         this.distancey = 0;
-        this.rotateangle = rotateanglelist[tank.directionstatus];
+        this.rotateangle = rotateanglelist[tank.directionstatus] ;
         this.init();
     }
-    init() {
-        this.x = tank.movex + tank.size().width * 0.42;
-        this.y = tank.movey - tank.size().width * 0.38;
+    init(){
+        this.x = tank.movex + tank.size().width * 0.42 ;
+        this.y = tank.movey - tank.size().width * 0.38 ;
         //taransform으로 위치를 잡기때문에 y좌표는 바텀 기준으로 값을 빼줘야 함 (transform y는 bottom부터 아래로 내려가면 +)
         this.distancex = this.x;
         this.distancey = this.y;
@@ -111,28 +138,32 @@ class Bullet {
         //각도 조절
         // this.bl.style.transform = `rotate(${this.rotateangle}deg)`;
         //별도로 div 하나 더 생성할 필요 없이 transform 할 때 rotate를 뒤에 입력해주면 됨
+        
         this.parentNode.appendChild(this.el);
         //.game에 자식으로 생성해줘야 함
         // this.el.appendChild(this.bl);
     }
-    moveBullet() {
+    moveBullet(){
+        
         this.distancex += this.speed * Math.cos(degtorad(this.rotateangle));
         this.distancey -= this.speed * Math.sin(degtorad(this.rotateangle));
+
         this.el.style.transform = `translate3d(${this.distancex}px, ${this.distancey}px, 0) rotate(${this.rotateangle}deg)`;
+
         this.crashBullet();
     }
-    position() {
-        return {
-            left: this.el.getBoundingClientRect().left,
-            right: this.el.getBoundingClientRect().right,
-            top: gameProp.screenHeight - this.el.getBoundingClientRect().top,
-            bottom: gameProp.screenHeight - this.el.getBoundingClientRect().top - this.el.getBoundingClientRect().height
-        };
-    }
-    crashBullet() {
-        if (this.position().left > gameProp.screenWidth || this.position().right < 0 ||
-            this.position().top > gameProp.screenHeight || this.position().bottom < 0) {
-            this.el.remove();
-        }
-    }
+    position(){
+		return{
+			left: this.el.getBoundingClientRect().left,
+			right: this.el.getBoundingClientRect().right,
+			top: gameProp.screenHeight - this.el.getBoundingClientRect().top,
+			bottom: gameProp.screenHeight - this.el.getBoundingClientRect().top - this.el.getBoundingClientRect().height
+		}
+	}   
+	crashBullet(){
+        if(this.position().left > gameProp.screenWidth || this.position().right < 0 || 
+        this.position().top > gameProp.screenHeight || this.position().bottom < 0 ){
+			this.el.remove();
+		}
+	}
 }
--- coding/code/ts/game.ts
+++ coding/code/ts/game.ts
@@ -1,72 +1,83 @@
 const key = {
-    keyDown: {},
-    keyValue: {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down',
-        88: 'attack'
+    keyDown : {},
+    keyValue : {
+        37 : 'left',
+        38 : 'up',
+        39 : 'right',
+        40 : 'down',
+        88 : 'attack'
     }
-};
+}
+
 const rotateanglelist = {
-    'up': 0,
-    'up_left': 315,
+    'up' : 0,
+    'up_left' : 315,
     'left': 270,
     'left_down': 225,
-    'down': 180,
-    'down_right': 135,
+    'down' : 180,
+    'down_right' : 135,
     'right': 90,
-    'right_up': 45
-};
+    'right_up' : 45
+}
+
 const bulletComProp = {
-    launch: false,
+    launch: false ,
     arr: []
-};
+}
+
 const gameProp = {
-    screenWidth: window.innerWidth,
-    screenHeight: window.innerHeight
-};
-const gameBackground = {
-    gameBox: document.querySelector('.game')
-};
+    screenWidth : window.innerWidth,
+    screenHeight : window.innerHeight
+}
+
+const gameBackground ={
+	gameBox: document.querySelector('.game') as HTMLElement | null
+}
+
 const renderGame = () => {
     tank.keyMotion();
     setGameBackground();
-    bulletComProp.arr.forEach((arr, i) => {
+
+    bulletComProp.arr.forEach((arr,i) => {
         arr.moveBullet();
-    });
+    })
     window.requestAnimationFrame(renderGame);
     // requestAnimationFrame 은 리페인트 이전에 실행할 콜백함수를 받아 다음 리페인트가 진행되기 전에 애니메이션을 업데이트하는 함수를 호출하게 하도록 함, 보통 1호 60회 지원
     // 이는 재귀함수를 통해 계속 반복할 수 있도록 하여 사용할 수 있다.
-};
+}
+
 const setGameBackground = () => {
-    let parallaxX = Math.min(0, (tank.movex - gameProp.screenWidth / 3) * -1);
-    let parallaxY = Math.max(0, (tank.movey + gameProp.screenWidth / 4) * -1);
-    gameBackground.gameBox.style.transform = `translate3d(${parallaxX}px,${parallaxY}px,0)`;
-};
+	let parallaxX = Math.min(0, (tank.movex-gameProp.screenWidth/3) * -1);
+    let parallaxY = Math.max(0, (tank.movey+gameProp.screenWidth/4) * -1 );
+
+	gameBackground.gameBox.style.transform = `translate3d(${parallaxX}px,${parallaxY}px,0)`;
+}
+
 const widowEvent = () => {
     window.addEventListener('keydown', e => {
         // console.log('키다운' + e.which);
         key.keyDown[key.keyValue[e.which]] = true;
         // console.log(key.keyDown);
-    });
+    })
     window.addEventListener('keyup', e => {
         key.keyDown[key.keyValue[e.which]] = false;
         // console.log(key.keyDown);
-    });
+    })
     window.addEventListener('resize', e => {
-        gameProp.screenWidth = window.innerWidth;
-        gameProp.screenHeight = window.innerHeight;
+		gameProp.screenWidth = window.innerWidth;
+		gameProp.screenHeight = window.innerHeight;
         // console.log(gameProp.screenWidth);
-    });
-};
-const loadImg = () => {
-    const preLoadImgSrc = ['../../lib/images/tank_move.png', '../../lib/images/tank_attack.png'];
+	});
+}
+
+const loadImg = ()=> {
+    const preLoadImgSrc = ['../../lib/images/tank_move.png','../../lib/images/tank_attack.png'];
     preLoadImgSrc.forEach(arr => {
         const img = new Image();
         img.src = arr;
-    });
-};
+    })
+}
+
 let tank;
 const init = () => {
     tank = new Tank('.tank');
@@ -74,7 +85,8 @@ const init = () => {
     loadImg();
     widowEvent();
     renderGame();
-};
+}
+
 window.onload = () => {
     init();
-};
+}
